d131616e2d4fbd22097d708cd95ad68188daf721
Jukeysz <scaglioni.ju@gmail.com>
2024-05-16 17:32:31 -0300

    style adjustment

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,8 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+    client = TestClient(app)  # Arrange
 
-    response = client.get("/")
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"message": "Olá Mundo!"}
+    response = client.get("/")  # Act
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {"message": "Olá Mundo!"}  # Assert
